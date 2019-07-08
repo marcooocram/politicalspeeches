bd46514811e8f3e5191bf961e6742dfa197ab657
molfem01 <Marco.Molfese@stepstone.com>
2019-07-08 21:09:30 +0200

    adding documentation

--- src/test/java/controllers/SpeechEvaluationTest.java
+++ src/test/java/controllers/SpeechEvaluationTest.java
@@ -25,30 +25,30 @@ public class SpeechEvaluationTest {
     private static final String LEAST_WORDY = "leastWordy";
 
     @Before
-    public void setup(){
-         evaluationServiceMock  = mock(EvaluationService.class);
-         speechEvaluation = new SpeechEvaluation(evaluationServiceMock);
+    public void setup() {
+        evaluationServiceMock = mock(EvaluationService.class);
+        speechEvaluation = new SpeechEvaluation(evaluationServiceMock);
     }
 
     @Test
     public void getEvaluationHappyPath() {
         //given
         List<String> urls = Collections.emptyList();
-        Evaluation evaluation = new Evaluation(MOST_SPEECHES,MOST_SECURITY,LEAST_WORDY);
+        Evaluation evaluation = new Evaluation(MOST_SPEECHES, MOST_SECURITY, LEAST_WORDY);
         when(evaluationServiceMock.evaluate(urls)).thenReturn(evaluation);
 
         //when
         ResponseEntity<Object> response = speechEvaluation.getEvaluation(urls);
 
         //then
-        assertThat(response.getStatusCode(),is(HttpStatus.OK));
-        assertThat(((Evaluation)response.getBody()).getMostSpeeches(),is(MOST_SPEECHES));
-        assertThat(((Evaluation)response.getBody()).getMostSecurity(),is(MOST_SECURITY));
-        assertThat(((Evaluation)response.getBody()).getLeastWordy(),is(LEAST_WORDY));
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+        assertThat(((Evaluation) response.getBody()).getMostSpeeches(), is(MOST_SPEECHES));
+        assertThat(((Evaluation) response.getBody()).getMostSecurity(), is(MOST_SECURITY));
+        assertThat(((Evaluation) response.getBody()).getLeastWordy(), is(LEAST_WORDY));
     }
 
     @Test
-    public void getEvaluationShouldReturnBadRequest(){
+    public void getEvaluationShouldReturnBadRequest() {
         //given
         List<String> urls = Collections.emptyList();
         when(evaluationServiceMock.evaluate(urls)).thenThrow(CSVParsingException.class);
@@ -57,12 +57,12 @@ public class SpeechEvaluationTest {
         ResponseEntity<Object> response = speechEvaluation.getEvaluation(urls);
 
         //then
-        assertThat(response.getStatusCode(),is(HttpStatus.BAD_REQUEST));
+        assertThat(response.getStatusCode(), is(HttpStatus.BAD_REQUEST));
     }
 
 
     @Test
-    public void getEvaluationShouldReturnInternalServerError(){
+    public void getEvaluationShouldReturnInternalServerError() {
         //given
         List<String> urls = Collections.emptyList();
         when(evaluationServiceMock.evaluate(urls)).thenThrow(RuntimeException.class);
@@ -71,8 +71,6 @@ public class SpeechEvaluationTest {
         ResponseEntity<Object> response = speechEvaluation.getEvaluation(urls);
 
         //then
-        assertThat(response.getStatusCode(),is(HttpStatus.INTERNAL_SERVER_ERROR));
+        assertThat(response.getStatusCode(), is(HttpStatus.INTERNAL_SERVER_ERROR));
     }
-
-
 }
